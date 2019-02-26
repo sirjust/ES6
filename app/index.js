056f7b8cf247ceeb1bba8efef4349cc92f37679f
SirJUST <theoperaking@yahoo.com>
2019-02-25 22:13:46 -0500

    spreadOperator, restParameters, and destructure

--- app/index.js
+++ app/index.js
@@ -1,5 +1,53 @@
 //variables();
-templates();
+//templates();
+//spreadOperator();
+//restParameters();
+//destructure();
+
+function destructure() {
+    let z = [4,5,6];
+    //extract these numbers into separate variables
+    let [four, five] = z;
+
+    // this is the old way
+    // let four = z[0];
+    // let five = z[1];
+    console.log(four, five);
+
+    let animals = ['Simba', 'Zazu', 'Ed'];
+    let [lion, bird] = animals;
+    console.log(lion, bird);
+
+    let king = {name: 'Mufasa', kids: 1};
+    // this is the old way
+    // let name = king.name;
+    // let kids = king.kids;
+
+    let {name, kids} = king;
+    console.log(name, kids);
+
+    let son = {sonName: 'Simba', parents: 2};
+    let sonName, parents;
+    // be careful of assigning first.. in order to make it work we need parentheses on the destructure line
+    ({sonName, parents} = son);
+    console.log(sonName, parents);
+}
+
+function restParameters(){
+    function collect(...a){
+        console.log(a);
+    }
+    collect(1,2,3,4,5);
+    collect(3,6,9,12,15);
+}
+
+function spreadOperator(){
+    let a = [20,30,40];
+    let b = [10,...a,50];
+    let c = ['Dana', 'Eric', 'Frank'];
+    let d = ['Alice', 'Bob', 'Carl', ...c];
+    console.log(b, d);
+}
 
 function variables() {
     const limit = 200;
